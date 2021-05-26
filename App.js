4015aa8c8ea4203c1791be5ac8623267dc352f59
nagamunireddy <nag.bmsit@gmail.com>
2021-05-26 15:53:01 +0530

    Second commit

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.topView}>
+          <Text> Search </Text>
+        </View>
+        <View style={styles.bottomView}>
+          <Text> Filter </Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  topView: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  bottomView: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "yellow",
+  },
+  sideView: {
+    flex: 1,
   },
 });
